9f6cb5d0eecfe9f2721e0f365fd894b3b1f5194b
Mehmet Onur Sagir <onuur.s@gmail.com>
2021-01-25 15:52:47 +0100

    test: add test routes overview

--- e2e-env/index.tsx
+++ e2e-env/index.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import routes from '../packages/*/tests/e2e/routes.tsx';
 import { render } from 'react-dom';
-import { Router } from '@reach/router';
+import { Link, Router } from '@reach/router';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <>
+      <h1>E2E Routes overview</h1>
+      {Object.entries(routes).map(([packageName, routeObject]: [string, any]) => {
+        return (
+          <div>
+            <h3>{packageName}</h3>
+            <ul>
+              {Object.keys(routeObject.default).map((route) => {
+                return (
+                  <li key={route}>
+                    <Link to={route}>{route}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 render(
   <Router>
+    <Home path="/" />
     {Object.values(routes).map((routeObject: any) => {
       return Object.entries(routeObject.default).map(([path, Component]: [string, any]) => {
         return <Component path={path} />;
